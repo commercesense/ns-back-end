c192b38862675db21fbe069f48d7c77d29c2e879
Marcelino Raygoza <marc@commercesense.io>
2024-10-21 23:04:05 -0700

    all cors

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(bodyParser.json());
-app.use(cors());
+//app.use(cors());
 
 // Define API routes
 app.post('/submit', (req, res) => {
